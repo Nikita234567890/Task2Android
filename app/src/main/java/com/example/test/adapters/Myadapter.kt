91f4a926df18ebdb5f51a87d88c024e8945f682a
Nikita234567890 <117517437+Nikita234567890@users.noreply.github.com>
2022-12-16 19:49:21 +0300

    Completed codelab1

--- app/src/main/java/com/example/test/adapters/Myadapter.kt
+++ app/src/main/java/com/example/test/adapters/Myadapter.kt
@@ -16,7 +16,6 @@ class Myadapter(
 
     class ItemViewHolder(private val view: View): RecyclerView.ViewHolder(view) {
         val textView: TextView = view.findViewById(R.id.item_title)
-        val imageView: ImageView = view.findViewById(R.id.item_image)
     }
 
 
@@ -31,7 +30,6 @@ class Myadapter(
     override fun onBindViewHolder(holder: ItemViewHolder, position: Int) {
         val item = dataset[position]
         holder.textView.text = context.resources.getString(item.stringResourceId)
-        holder.imageView.setImageResource(item.imageResourceId)
     }
     override fun getItemCount() = dataset.size
 }
